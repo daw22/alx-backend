be36ce82ceea1f0381b693f3fdd4c8e672574db6
dawit <dawitxxd@gmail.com>
2024-08-27 16:07:54 +0300

    type annotation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,12 +7,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/', strict_slashes=False)
+def home() -> str:
     """
     Home route
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 if __name__ == "__main__":
     app.run()
