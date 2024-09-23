38cf3712d1a8f5f850779524830b136e76cd022e
dawit <dawitxxd@gmail.com>
2024-09-23 14:36:58 +0300

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,16 @@
+import redis, { createClient } from 'redis';
+
+const client = createClient();
+
+client.hset('HolbertonSchools', 'Portland', '50', redis.print);
+client.hset('HolbertonSchools', 'Seattle', '80', redis.print);
+client.hset('HolbertonSchools', 'New York', '20', redis.print);
+client.hset('HolbertonSchools', 'Bogota', '20', redis.print);
+client.hset('HolbertonSchools', 'Cali', '40', redis.print);
+client.hset('HolbertonSchools', 'Paris', '2', redis.print);
+
+client.hgetall('HolbertonSchools', (err, res) => {
+  if (!err) {
+    console.log(res);
+  }
+});
